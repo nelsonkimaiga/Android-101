a7be46ce30d71ed72d571350d4db63f3c4d09b6f
Nelson Kimaiga <nelsonkimaiga@aol.com>
2015-10-11 16:42:27 +0300

    Button listerners for button onClick method

--- KimaigaAndroid/app/src/main/java/com/example/kimaiga/kimaigaandroid/MyActivity.java
+++ KimaigaAndroid/app/src/main/java/com/example/kimaiga/kimaigaandroid/MyActivity.java
@@ -4,13 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MyActivity extends AppCompatActivity {
+
+Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        button = ( Button ) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.onClickListener(){
+            //@Override
+                    public void onClick(View v){
+                button.setText("I am clicked");
+            }
+
+
+        });
+
     }
 
     @Override
